4b7a8219167b0eaa34e9835001b0c973c8daadda
zaime ali <zaimeali1997@gmail.com>
2020-11-18 19:09:41 +0500

    Constraints on Params

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/{id?}', function ($id = 2) {
+    return 'Blog Post ' . $id;
+})->where([
+    'id' => '[0-9]+',
+])->name('posts.show');
