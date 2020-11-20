d8463231ad545531b7a17e66ae3bf2ba27decceb
zaime ali <zaimeali1997@gmail.com>
2020-11-20 19:38:53 +0500

    Lecture 29 Done

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,17 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('home.index');
-})->name('home.index');
+// Route::get('/', function () {
+//     return view('home.index');
+// })->name('home.index');
 
-Route::get('/contact', function () {
-    return view('home.contact');
-})->name('home.contact');
+// Route::get('/contact', function () {
+//     return view('home.contact');
+// })->name('home.contact');
+
+// Refactoring the above two routes
+Route::view('/', 'home.index')->name('home.index');
+Route::view('/contact', 'home.contact')->name('home.contact');
 
 // Route::get('/posts/{id?}', function ($id = 2) {
 //     return 'Blog Post ' . $id;
